13eed9a4d23aaca31da32f68067245b7523352ca
Bedanta1806 <bedanta2026.ktm@ullens.edu.np>
2025-04-26 21:27:37 +0545

    Add files via upload

---
+++ p.py
@@ -0,0 +1,10 @@
+fname= "BEDANTA "
+lname= "LAUDARI"
+fullname= fname + lname
+print (fullname)
+e= "MOTE "*5
+print (e)
+print("THE WORD IS LONG:",len(e))
+print("FIRST LETTER OF STRING", e[0])
+print("LAST LETTER OF STRING", e[19])
+print("FIRST THREE LETTERS", e[0:3])
